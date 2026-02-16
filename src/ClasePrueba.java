d1040aec692fba6117294577519b26a21e30c396
Camila Mejia. <mariac.mejiap@uqvirtual.edu.co>
2026-02-15 20:28:40 -0500

    commit nueva actualización

--- src/ClasePrueba.java
+++ src/ClasePrueba.java
@@ -3,6 +3,7 @@ public class ClasePrueba {
     public static void main(String[] args) {
             System.out.println("Prueba de commit funcionando correctamente");
         System.out.println("Funciona correctamente");
+        System.out.println("Nueva actualización");
         }
 
 }
