d1badeef8669c20a7d1c0e719ce366355ec9779b
estebanbuitragoc-star <esteban.buitragoc@uqvirtual.edu.co>
2026-02-16 00:08:59 -0500

    Prueba 2 durante video

--- src/CalificacionInteractiva.java
+++ src/CalificacionInteractiva.java
@@ -1,14 +1,20 @@
+import java.util.Scanner;
+
 public class CalificacionInteractiva {
-    public static void main(String[] args) {
-        // Nota fija en la variable
-        double profesorRaul = 3.5;
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Profesor Raúl, ingrese la nota del trabajo: ");
+        double profesorRaul = entrada.nextDouble();
 
         System.out.println("El profesor Raúl nos puso la nota: " + profesorRaul);
 
-        if (profesorRaul >= 4) {
-            System.out.println(":) ¡Carita feliz! Vamos bien");
+        if (profesorRaul >= 4){
+            System.out.println(" :) ¡Carita feliz! Vamos bien" );
         } else {
-            System.out.println(":( Carita triste...");
+            System.out.println(" :( Carita triste...");
         }
+
+        entrada.close();
     }
 }
