e404e1f9838973430dfea55d2e1d77e6f2a0748b
estebanbuitragoc-star <esteban.buitragoc@uqvirtual.edu.co>
2026-02-15 20:25:10 -0500

    Prueba de commit clase prueba

--- src/ClasePrueba.java
+++ src/ClasePrueba.java
@@ -2,6 +2,7 @@ public class ClasePrueba {
 
     public static void main(String[] args) {
             System.out.println("Prueba de commit funcionando correctamente");
+        System.out.println("Funciona correctamente");
         }
 
 }
